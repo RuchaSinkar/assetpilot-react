29bab77d51457eb0c7d81a530057812867a7ade8
Rucha <ruchkar2006@gmail.com>
2025-09-20 19:04:03 +0530

    borrow functionality

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,13 @@ import Dashboard from './pages/Dashboard'
 import Report from './pages/Report'
 import Signin from './pages/Signin' 
 import {useNavigate} from 'react-router-dom';
+import Borrow from './components/Borrow'
 function App() {
 
   return (
-
     <>
       <Home/>
+      <Borrow />
       <Dashboard/>
       <Report/>
       <Signin/>
